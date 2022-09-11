97bd51d95ea41fb4d78a65d3b5f0e0b47c616146
junepro <57428866+junepro@users.noreply.github.com>
2022-09-11 17:47:00 +0900

    회원 수정 api

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -2,11 +2,10 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -21,17 +20,31 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    /**
+     * 등록 V2: 요청 값으로 Member 엔티티 대신에 별도의 DTO를 받는다.
+     */
     @PostMapping("/api/v2/members")
-    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid
+                                             CreateMemberRequest request) {
         Member member = new Member();
         member.setName(request.getName());
-
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
-        }
+    }
+
+    /**
+     * 수정 API
+     */
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
 
     @Data
-    private class CreateMemberResponse {
+    static class CreateMemberResponse {
         private Long id;
 
         public CreateMemberResponse(Long id) {
@@ -40,7 +53,19 @@ public class MemberApiController {
     }
 
     @Data
-    private class CreateMemberRequest {
+    static class CreateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
+    @Data
+    static class UpdateMemberRequest {
         private String name;
     }
 }
